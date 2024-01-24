1e1786418fb46b37da376be6c3e3fd34a1a7c49b
youhyeoneee <khukyh2018@khu.ac.kr>
2024-01-24 10:38:12 +0900

    refactor: 불필요한 Import 삭제

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import HelloWorld from './components/HelloWorld';
-import CaptionImage from './components/CaptionImage';
-import BlinkCompnent from './components/BlinkComponent.js';
-import CountComponent from './components/CountComponent.js';
 import { useState } from 'react';
-import FocusInputButton from './components/RegisterInputButton.js';
-import ChangeColor from './components/ChangeColor.js';
 import TodoList from './components/TodoList.js';
 
 
